74f0936a3cf111aa5d6af7e58ee207b6d3e8c300
santiago <santiago.ottolini97@hotmail.com>
2021-05-26 08:58:24 -0300

    Validation for  add character and create an interface for character

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -2,6 +2,7 @@ import { IResolvers } from "graphql-tools";
 import data from "../../data/data.json";
 import { Db, ObjectId } from "mongodb";
 import { CHARACTER_COLLECTION, GAME_COLLECTION } from "../../mongo/collections";
+import { ICharacter } from "../../interfaces/ICharacter";
 
 export const characterResolver: IResolvers = {
   Query: {
@@ -12,48 +13,61 @@ export const characterResolver: IResolvers = {
         console.log(error);
       }
     },
-    getCharacter(root: void, args: any) {
-      const [find] = data.characters.filter((ch) => ch._id === args._id);
+    async getCharacter(root: void, args: any, context: Db) {
+      const find = await context
+        .collection(CHARACTER_COLLECTION)
+        .findOne({ _id: new ObjectId(args._id) });
       return find;
     },
   },
   Mutation: {
     async createCharacter(root: void, args: any, context: Db) {
       try {
+        const regexp = new RegExp(args.character.name, "i");
+        const exists = await context
+          .collection(CHARACTER_COLLECTION)
+          .findOne({ name: args.character.name });
+        if (exists) {
+          return "Character already exist";
+        }
         await context
           .collection(CHARACTER_COLLECTION)
           .insertOne(args.character);
-        data.characters.push(args.character);
+
         return "Character added successfully";
       } catch (error) {
         console.log(error);
       }
     },
-    async editCharacter(root: void, args: any, context: Db) {
+    async editCharacter(
+      root: void,
+      { _id, character }: { _id: string; character: ICharacter },
+      context: Db
+    ) {
       try {
         const exist = await context
           .collection(CHARACTER_COLLECTION)
-          .findOne({ _id: new ObjectId(args._id) });
+          .findOne({ _id: new ObjectId(_id) });
 
         if (exist) {
           await context
             .collection(CHARACTER_COLLECTION)
-            .updateOne(
-              { _id: new ObjectId(args._id) },
-              { $set: args.character }
-            );
+            .updateOne({ _id: new ObjectId(_id) }, { $set: character });
 
-            return "Character updated successfully";
+          return "Character updated successfully";
         }
+
+        throw new Error("Character does not exist");
       } catch (error) {
         console.log(error);
+        return error.message;
       }
     },
   },
   Character: {
-    async games(parent: any, args: void, context: Db) {
+    async games(parent: ICharacter, args: void, context: Db) {
       const gameList = parent.games.map(
-        async (gameId: string) =>
+        async (gameId) =>
           await context
             .collection(GAME_COLLECTION)
             .findOne({ _id: new ObjectId(gameId) })
